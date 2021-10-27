8f65709b29015e2b9cbc2071acc4921a12832781
laks0 <laksjoaquin@gmail.com>
2021-10-26 21:03:47 -0300

    Migrado a VUE3

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<Presupuesto/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Presupuesto from "./components/Presupuesto"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: "App",
+	components: {
+		Presupuesto
+	}
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import "@progress/kendo-ui"
+import "@progress/kendo-theme-default/dist/all.css"
+
 createApp(App).mount('#app')
