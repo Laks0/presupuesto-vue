1e979fda9297b54142c53568bfa0ea2076f4df05
laks0 <laksjoaquin@gmail.com>
2021-11-24 01:58:14 -0300

    Funcionalidad de login que se conecta a la API

--- src/components/Principal/Login/Login.vue
+++ src/components/Principal/Login/Login.vue
@@ -1,12 +1,16 @@
 <template>
 	<k-form @submit="handleSubmit">
 		<login-content/>
+
+		<p v-if="emailError">El mail no existe</p>
+		<p v-if="passError">La contraseña es incorrecta</p>
+		<p v-if="success">Cuenta de {{nombre}}</p>
 	</k-form>
 </template>
 
 <script>
 import http from "../../../http-common.js";
-//import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { Form } from "@progress/kendo-vue-form";
 import LoginContent from "./LoginContent.vue";
 
@@ -18,22 +22,34 @@ export default {
 	},
 	methods: {
 		handleSubmit (dataItem) {
+			this.emailError = false;
+			this.passError = false;
+			this.success = false;
 			http.get(`/usuario/${dataItem.email}`)
 				.then((res) => {
 					if (res.data === null) {
-						console.log("El mail no existe");
+						this.emailError = true;
 						return;
 					}
-//					bcrypt.compare(dataItem.data.contraseña, res.data.contraseña, (err, comp) => {
-//						if (err) {
-//							console.error(err);
-//							return;
-//						}
-//						console.log(comp ? dataItem.data.nombre : "Contraseña incorrecta");
-//					})
+					if (!bcrypt.compareSync(dataItem.contraseña, res.data.contraseña)) {
+						this.passError = true;
+						return;
+					}
+
+					this.success = true;
+					this.nombre = res.data.nombre;
 				})
 				.catch((err) => console.error(err));
 		}
-	}
+	},
+
+	data: function() {
+		return {
+			emailError: false,
+			passError: false,
+			success: false,
+			nombre: ""
+		};
+	},
 }
 </script>
