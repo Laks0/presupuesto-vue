c5bd25c7b24a3ade055eab2f9399a4783441d8e6
laks0 <laksjoaquin@gmail.com>
2021-12-20 19:25:10 -0300

    Revert "[0.1.1] Intento de solución de headers en Heroku"
    This reverts commit 587896e5838443d2f02e6a82ea905f26797c153b.

--- src/http-common.js
+++ src/http-common.js
@@ -5,6 +5,5 @@ export default axios.create({
 	baseURL: process.env.VUE_APP_API,
 	headers: {
 		"Content-type": "application/json",
-		"Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE",
 	}
 });
